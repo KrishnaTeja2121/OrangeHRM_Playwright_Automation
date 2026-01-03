d3068c06abdeef00e66c0bf21506c188826b6a31
KrishnaTeja2121 <37261298+KrishnaTeja2121@users.noreply.github.com>
2026-01-03 19:20:08 +0530

    Updated data and fixtures

---
+++ fixtures/common-fixture.ts
@@ -0,0 +1,18 @@
+import { test as baseTest } from './pom-fixture'
+import CommonUtils from '../utils/CommonUtils'
+import CommonApiUtils from '../utils/CommonApiUtils'
+
+
+type CommonFixtureType = {
+    commonUtils: CommonUtils,
+    commonApiUtils: CommonApiUtils
+}
+
+export const test = baseTest.extend<CommonFixtureType>({
+    commonUtils: async ({ }, use) => {
+        use(new CommonUtils())
+    },
+    commonApiUtils: async({request}, use)=>{
+        use(new CommonApiUtils(request))
+    }
+})
--- tests/api-test/restful-booker-app.spec.ts
+++ tests/api-test/restful-booker-app.spec.ts
@@ -1,7 +1,9 @@
+import { expect } from '@playwright/test';
 import {test} from '../../fixtures/pom-fixtures'
 
 test('API Test', async ({request})=>{
     const bookingIds=await request.get('https://restful-booker.herokuapp.com/booking/');
     console.log(await bookingIds.json());
+    expect(bookingIds.status().toFixed(200));
 
 })
